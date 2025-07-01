416fbf977745d20d9c1a89fb8770bfd0b09b65a1
Hari Upreti <hariupreti679@gmail.com>
2025-07-01 11:38:54 +0545

    Fixed issue related to users and permission, also added remote access for root user

--- mysql-init/02_set_permissions.sql
+++ mysql-init/02_set_permissions.sql
@@ -1,8 +1,13 @@
 -- Give userone limited permissions
 GRANT SELECT, INSERT, UPDATE, DELETE ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER_ONE}'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER_ONE}'@'localhost';
 
 -- Give adminuser full privileges on the database
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_ADMIN_USER}'@'%';
+
+-- Allowing remote access for root user using password.
+CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 
 -- Apply the changes
 FLUSH PRIVILEGES;
